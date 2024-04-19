6ba48377ff7d91516380b7fdbf49697528e16e25
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2024-04-19 11:10:45 -0500

    Adding error handling

--- lambda.py
+++ lambda.py
@@ -7,28 +7,34 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('CryptoPrecios')
 
 def lambda_handler(event, context):
-    # Obtener precios de BTC y ETH desde CoinMarketCap API
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-    params = {
-        'symbol': 'BTC,ETH'
-    }
-    headers = {
-        'ae927795-11bd-4aca-917f-eef3bc5237fa'
-    }
-    response = requests.get(url, params=params, headers=headers).json()
-
-    # Formatear datos y almacenarlos en DynamoDB
-    for crypto in ['BTC', 'ETH']:
-        precio = response['data'][crypto]['quote']['USD']['price']
-        fecha = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        item = {
-            'CryptoId': crypto,
-            'Fecha': fecha,
-            'Precio': precio
+    try:
+        # Obtener precios de BTC y ETH desde CoinMarketCap API
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+        params = {
+            'symbol': 'BTC,ETH'
         }
-        table.put_item(Item=item)
+        headers = {
+            'ae927795-11bd-4aca-917f-eef3bc5237fa'
+        }
+        response = requests.get(url, params=params, headers=headers).json()
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Precios actualizados correctamente')
-    }
+        # Formatear datos y almacenarlos en DynamoDB
+        for crypto in ['BTC', 'ETH']:
+            precio = response['data'][crypto]['quote']['USD']['price']
+            fecha = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            item = {
+                'CryptoId': crypto,
+                'Fecha': fecha,
+                'Precio': precio
+            }
+            table.put_item(Item=item)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Precios actualizados correctamente')
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error: ' + str(e))
+        }
